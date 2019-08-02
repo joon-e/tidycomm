afa1b0ef038fc87e4c216118268aa3620330cb79
Julian Unkel <julian.unkel@gmail.com>
2019-08-02 19:22:29 +0200

    Added correlation functions

---
+++ R/correlation.R
@@ -0,0 +1,101 @@
+#' Compute correlation test
+#'
+#' Computes a correlation test for a two variables
+#'
+#' @param var_comb A character vector containing the name of two variables
+#' @param data a [tibble][tibble::tibble-package]
+#' @param method a character string indicating which correlation coefficient
+#'   is to be computed. One of "pearson" (default), "kendall", or "spearman"
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family correlations
+correlation_test <- function(var_comb, data, method) {
+  x <- var_comb[[1]]
+  y <- var_comb[[2]]
+  xvar <- data[[x]]
+  yvar <- data[[y]]
+
+  if (any(!is.numeric(xvar), !is.numeric(yvar))) {
+    warning(glue::glue("At least one of {x} and {y} is not numeric, skipping computation."),
+            call. = FALSE)
+    return()
+  }
+
+  cor_test <- cor.test(xvar, yvar, method = method)
+
+  if (method == "pearson") {
+    name <- "r"
+  } else if (method == "kendall") {
+    name <- "tau"
+  } else if (method == "spearman") {
+    name <- "rho"
+  }
+
+  tibble(
+    x = x,
+    y = y,
+    !!name := cor_test$estimate,
+    df = ifelse(is.null(cor_test$parameter),
+                NA, cor_test$parameter),
+    p = cor_test$p.value
+  )
+}
+
+#' Compute correlation coefficients
+#'
+#' Computes correlation coefficients for all combinations of the specified
+#' variables. If no variables are specified, all numeric (integer or double)
+#' variables are used.
+#'
+#' @param data a [tibble][tibble::tibble-package]
+#' @param ... Variables to compute correlations for (column names). Leave empty
+#'   to compute for all numeric variables in data.
+#' @param method a character string indicating which correlation coefficient
+#'   is to be computed. One of "pearson" (default), "kendall", or "spearman"
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family correlations
+#'
+#' @export
+correlate <- function(data, ..., method = "pearson") {
+
+  if (!method %in% c("pearson", "kendall", "spearman")) {
+    stop('method must be one of "pearson", "kendall" or "spearman"',
+         call. = FALSE)
+  }
+
+  vars <- grab_vars(data, rlang::enquos(...))
+
+  var_strings <- purrr::map_chr(vars, rlang::as_label)
+  var_combs <- combn(var_strings, 2, simplify = FALSE)
+  purrr::map_dfr(var_combs, correlation_test, data, method)
+}
+
+#' Create correlation matrix
+#'
+#' Turns the tibble exported from \code{\link{correlate}} into a correlation
+#' matrix.
+#'
+#' @param data a [tibble][tibble::tibble-package] returned from \code{\link{correlate}}
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family correlation
+#'
+#' @export
+to_correlation_matrix <- function(data) {
+
+  estimate <- names(data)[3]
+
+  data %>%
+    dplyr::select(x = 1, y = 2, cor = 3) %>%
+    dplyr::bind_rows(
+      data %>%
+        dplyr::select(x = 1, y = 2, cor = 3) %>%
+        dplyr::rename(x = y, y = x)
+    ) %>%
+    tidyr::spread(y, cor, fill = 1) %>%
+    dplyr::rename(!!estimate := x)
+}
