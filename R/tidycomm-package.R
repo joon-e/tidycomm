309f5b8d21b0bde579b620abd00e773e1d4136f3
joon-e <julian.unkel@gmail.com>
2019-08-05 17:29:31 +0200

    Import functions from default packages

--- R/tidycomm-package.R
+++ R/tidycomm-package.R
@@ -1,4 +1,6 @@
 ## usethis namespace: start
 #' @importFrom tibble tibble
+#' @importFrom stats chisq.test cor cor.test median na.omit quantile sd t.test var
+#' @importFrom utils combn hasName
 ## usethis namespace: end
 NULL
