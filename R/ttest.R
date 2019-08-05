289394125dae4569a309957838ed3865ce19dee9
joon-e <julian.unkel@gmail.com>
2019-08-05 17:24:06 +0200

    Updated ttest documentations

--- R/ttest.R
+++ R/ttest.R
@@ -1,3 +1,32 @@
+#' Compute t-tests
+#'
+#' Computes t-tests for one group variable and specified test variables.
+#' If no variables are specified, all numeric (integer or double) variables are
+#' used.
+#'
+#' @param data a [tibble][tibble::tibble-package]
+#' @param group_var group variable (column name)
+#' @param ... test variables (column names). Leave empty to compute t-tests for
+#'   all numeric variables in data.
+#' @param var.equal a logical variable indicating whether to treat the two
+#'   variances as being equal. If `TRUE` then the pooled variance is used to
+#'   estimate the variance otherwise the Welch (or Satterthwaite) approximation
+#'   to the degrees of freedom is used. Defaults to `TRUE`.
+#' @param paired a logical indicating whether you want a paired t-test. Defaults
+#'   to `FALSE`.
+#' @param pooled_sd a logical indicating whether to use the pooled standard
+#'   deviation in the calculation of Cohen's d. Defaults to `TRUE`.
+#' @param levels optional: a vector of length two specifying the two levels of
+#'   the group variable.
+#' @param case_var optional: case-identifying variable (column name). If you
+#'   set `paired = TRUE`, specifying a case variable will ensure that data
+#'   are properly sorted for a dependent t-test.
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family t-test
+#'
+#' @export
 t_test <- function(data, group_var, ...,
                    var.equal = TRUE, paired = FALSE, pooled_sd = TRUE,
                    levels = NULL, case_var = NULL) {
@@ -28,7 +57,9 @@ t_test <- function(data, group_var, ...,
     } else if (length(levels) > 2) {
       warning(glue::glue("{group_var_str} has more than 2 levels, defaulting to first two ",
                          "({levels[1]} and {levels[2]}). ",
-                         "Consider filtering your data."), call. = FALSE)
+                         "Consider filtering your data ",
+                         "or setting levels with the levels argument"),
+              call. = FALSE)
       data <- data %>%
         dplyr::filter({{ group_var }} %in% levels[1:2]) %>%
         droplevels()
@@ -57,8 +88,15 @@ t_test <- function(data, group_var, ...,
 
 }
 
-
-#
+#' Compute t-test
+#'
+#' Computes and outputs a t-test for one test variable
+#'
+#' @inheritParams t_test
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family t-test
 compute_t_test <- function(test_var, data, group_var, levels,
                            var.equal, paired, pooled_sd) {
 
@@ -95,7 +133,20 @@ compute_t_test <- function(test_var, data, group_var, levels,
   )
 }
 
-
+#' Compute Cohen's d
+#'
+#' Computes the effect size estimate Cohen's d for two sets of numerical values
+#'
+#' @param x a (non-empty) numeric vector of data values.
+#' @param y a (non-empty) numeric vector of data values.
+#' @param pooled_sd a logical indicating whether to use the pooled standard
+#'   deviation in the calculation of Cohen's d. Defaults to `TRUE`.
+#' @param na.rm a logical value indicating whether NA values should be stripped
+#'  before the computation proceeds. Defaults to `TRUE`.
+#'
+#' @return a `dbl`
+#'
+#' @family t-test
 cohens_d <- function(x, y, pooled_sd = TRUE, na.rm = TRUE) {
 
   nx <- length(!is.na(x))
