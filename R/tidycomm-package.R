989d380bff82eb504eac6160013653e9b4b94ed8
Julian Unkel <julian.unkel@gmail.com>
2019-08-07 16:52:00 +0200

    Updated dependencies

--- R/tidycomm-package.R
+++ R/tidycomm-package.R
@@ -1,6 +1,6 @@
 ## usethis namespace: start
 #' @importFrom tibble tibble
-#' @importFrom stats chisq.test cor cor.test median na.omit quantile sd t.test var
+#' @importFrom stats chisq.test cor cor.test median na.omit quantile sd t.test var TukeyHSD aov as.formula
 #' @importFrom utils combn hasName
 ## usethis namespace: end
 NULL
