5e7037df224283f08d539ee09c153c821d632364
joon-e <julian.unkel@gmail.com>
2019-09-06 10:22:02 +0200

    Added tests for ICR functions

---
+++ tests/testthat/test-icr.R
@@ -0,0 +1,43 @@
+context("ICR: Main functions")
+
+test_that("unit-coder matrices are generated", {
+
+  ucm_string <- unit_coder_matrix(fbposts, post_id, coder_id, type)
+  ucm_int <- unit_coder_matrix(fbposts, post_id, coder_id, pop_elite)
+
+  expect_equal(dim(ucm_string), c(45, 6))
+  expect_equal(dim(ucm_int), c(45, 6))
+})
+
+test_that("compute_icr returns tibble", {
+
+  t_string <- compute_icr(type, fbposts, post_id, coder_id)
+  t_int <- compute_icr(pop_elite, fbposts, post_id, coder_id)
+
+  expect_true(tibble::is_tibble(t_string))
+  expect_true(tibble::is_tibble(t_int))
+
+  expect_equal(dim(t_string), c(1, 8))
+  expect_equal(dim(t_int), c(1, 8))
+})
+
+test_that("test_icr works with specified test variables", {
+
+  t1v <- test_icr(fbposts, post_id, coder_id, pop_elite)
+  t3v <- test_icr(fbposts, post_id, coder_id, pop_elite, pop_people, pop_othering)
+
+  expect_true(tibble::is_tibble(t1v))
+  expect_true(tibble::is_tibble(t3v))
+
+  expect_equal(dim(t1v), c(1, 8))
+  expect_equal(dim(t3v), c(3, 8))
+})
+
+test_that("test_icr works tests all variables if no test variables are specified", {
+
+  t <- test_icr(fbposts, post_id, coder_id)
+
+  expect_true(tibble::is_tibble(t))
+
+  expect_equal(dim(t), c(5, 8))
+})
