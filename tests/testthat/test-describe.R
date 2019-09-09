1d2f1e8e113998b95198a5a75d69bf322f6890f1
Julian Unkel <julian.unkel@gmail.com>
2019-09-09 22:13:12 +0200

    Updated describe tests

--- tests/testthat/test-describe.R
+++ tests/testthat/test-describe.R
@@ -1,3 +1,49 @@
+context("Describe")
+
+# Main function
+
 test_that("describe returns tibble", {
-  expect_true(tibble::is_tibble(describe(mtcars)))
+
+  t <- describe(WoJ, ethics_1, ethics_2, ethics_3)
+
+  expect_true(tibble::is_tibble(t))
+  expect_equal(dim(t), c(3, 13))
+})
+
+test_that("describe works without specifying variables", {
+
+  t <- describe(WoJ)
+
+  expect_true(tibble::is_tibble(t))
+  expect_equal(dim(t), c(11, 13))
+})
+
+test_that("describe works with tidyselect helpers", {
+
+  t <- describe(WoJ, tidyselect::starts_with("ethics"))
+
+  expect_true(tibble::is_tibble(t))
+  expect_equal(dim(t), c(4, 13))
+})
+
+# Skewness & Kurtosis helpers
+
+test_that("skewness returns correct results", {
+  x1 <- c(0:10)
+  x2 <- c(0:10, 50)
+  x3 <- c(rep(1, 5), 1:10)
+
+  expect_equal(skewness(x1), 0, tolerance = .0005)
+  expect_equal(skewness(x2), 2.717, tolerance = .0005)
+  expect_equal(skewness(x3), 0.569, tolerance = .0005)
+})
+
+test_that("kurtosis returns correct results", {
+  x1 <- c(0:10)
+  x2 <- c(0:10, 50)
+  x3 <- c(rep(1, 5), 1:10)
+
+  expect_equal(kurtosis(x1), 1.780, tolerance = .0005)
+  expect_equal(kurtosis(x2), 9.028, tolerance = .0005)
+  expect_equal(kurtosis(x3), 1.852, tolerance = .0005)
 })
