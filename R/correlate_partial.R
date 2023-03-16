82cfa1b7d9bccbd4e2c4def757e0f13629799b30
Lara Kobilke <100684892+LKobilke@users.noreply.github.com>
2023-03-16 14:48:47 +0100

    initial version of correlate_partial(...) alongside tests

---
+++ R/correlate_partial.R
@@ -0,0 +1,97 @@
+#' Compute partial correlation coefficients for three variables
+#'
+#' Computes pairwise partial correlations (pearson, kendall, spearman) for
+#' all combinations, i.e. pairs, of specified variables while controlling for the third variable.
+#'
+#' @param data a [tibble][tibble::tibble-package]
+#' @param ... three variables to compute partial correlations for (column names).
+#' @param method a character string indicating which partial correlation coefficient
+#'   is to be computed. One of "pearson" (default), "kendall", or "spearman"
+#'
+#' @return a [tibble][tibble::tibble-package]
+#'
+#' @family correlations
+#'
+#' @examples
+#' WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3)
+#'
+#' @export
+
+correlate_partial <- function(data, ..., method = "pearson") {
+
+  if (!method %in% c("pearson", "kendall", "spearman")) {
+    stop('Method must be one of "pearson", "kendall" or "spearman".',
+         call. = FALSE)
+  }
+
+  vars <- grab_vars(data, enquos(...), alternative = "none")
+
+  # basic checks: check whether three variables have been provided
+  if (length(vars) > 3) {
+    stop('The computation cannot be performed due to an excessive number of variables provided. Please provide exactly three variables.',
+         call. = FALSE)
+  }
+
+  if (length(vars) == 0) {
+    stop("No variable(s) given. Please provide exactly three variables.")
+  }
+
+  if (length(vars) < 3) {
+    stop('The computation cannot be performed because there are not enough variables provided. Please provide exactly three variables.',
+         call. = FALSE)
+  }
+
+  # basic checks: check whether data is grouped
+  if (dplyr::is.grouped_df(data)) {
+    warning("correlate_partial does not support grouped data. Groups will be dropped.",
+            call. = FALSE)
+    data <- dplyr::ungroup(data)
+  }
+
+  var_strings <- data %>%
+    dplyr::select(!!!vars) %>%
+    names()
+  var_combs <- combinat::permn(var_strings)
+  var_combs <- list(var_combs[[1]], var_combs[[2]], var_combs[[4]])
+  purrr::map_dfr(var_combs, correlation_partial_test, data, method)
+}
+
+
+
+###########################################
+correlation_partial_test <- function(var_comb, data, method) {
+  x <- var_comb[[1]]
+  y <- var_comb[[2]]
+  z <- var_comb[[3]]
+  xvar <- data[[x]]
+  yvar <- data[[y]]
+  zvar <- data[[z]]
+
+  # basic checks
+  if (any(!is.numeric(xvar), !is.numeric(yvar), !is.numeric(zvar))) {
+    stop(glue::glue("The computation cannot be performed because at least one of {x}, {y}, and {z} is not numeric."),
+         call. = FALSE)
+    return()
+  }
+
+  cor_partial_test <- ppcor::pcor.test(xvar, yvar, zvar, method = method)
+
+  if (method == "pearson") {
+    name <- "r"
+  } else if (method == "kendall") {
+    name <- "tau"
+  } else if (method == "spearman") {
+    name <- "rho"
+  }
+
+  tibble(
+    x = x,
+    y = y,
+    z = z,
+    !!name := cor_partial_test$estimate,
+    df = cor_partial_test$n - 2 - 1, # df formula: n - 2 - k,
+    # where k == number of vars we are conditioning on
+    p = ifelse(is.null(cor_partial_test$p.value),
+               NA, cor_partial_test$p.value)
+  )
+}
---
+++ tests/testthat/test-correlate_partial.R
@@ -0,0 +1,61 @@
+context("correlate_partial")
+
+# Main functions
+
+test_that("correlate_partial() returns tibble", {
+
+  t <- WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3)
+
+  expect_true(tibble::is_tibble(t))
+  expect_equal(dim(t), c(3, 6))
+})
+
+test_that("correlate_partial() returns tibble for other methods", {
+
+  t1 <- WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3, method = "spearman")
+  t2 <- WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3, method = "kendall")
+
+  expect_true(tibble::is_tibble(t1))
+  expect_true("rho" %in% names(t1))
+  expect_equal(dim(t1), c(3, 6))
+
+  expect_true(tibble::is_tibble(t2))
+  expect_true("tau" %in% names(t2))
+  expect_equal(dim(t2), c(3, 6))
+})
+
+test_that("correlate_partial() calculates correct results for Pearson correlations", {
+  result <- WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3)
+  partial_cor_numerator <- cor(WoJ$ethics_1, WoJ$ethics_2, method = "pearson") -
+                 cor(WoJ$ethics_1, WoJ$ethics_3, method = "pearson") *
+                 cor(WoJ$ethics_2, WoJ$ethics_3, method = "pearson")
+  partial_cor_denumerator <- sqrt((1-(cor(WoJ$ethics_1, WoJ$ethics_3, method = "pearson")^2))*(1-(cor(WoJ$ethics_3, WoJ$ethics_2, method = "pearson")^2)))
+  partial_cor <- partial_cor_numerator / partial_cor_denumerator
+  expect_equal(round(result$r[1], digits = 3), round(partial_cor, digits = 3))
+})
+
+test_that("correlate_partial() works with tidyselect helpers", {
+  expect_error(WoJ %>% correlate_partial(tidyselect::starts_with("ethics")),
+               "The computation cannot be performed due to an excessive number of variables provided. Please provide exactly three variables.")
+})
+
+## Possible errors
+
+test_that("Error is thrown when number of provided variables is less than three", {
+  expect_error(WoJ %>% correlate_partial(ethics_1, ethics_2),
+               "The computation cannot be performed because there are not enough variables provided. Please provide exactly three variables.")
+})
+
+test_that("Error is thrown when number of provided variables is more than three", {
+  expect_error(WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3, ethics_4),
+               "The computation cannot be performed due to an excessive number of variables provided. Please provide exactly three variables.")
+})
+
+test_that("Error is thrown when method is not one of 'pearson', 'kendall' or 'spearman'", {
+  expect_error(WoJ %>% correlate_partial(ethics_1, ethics_2, ethics_3, method = "wrong"),
+               "Method must be one of \"pearson\", \"kendall\" or \"spearman\".")
+})
+
+test_that("Error is thrown when non-numeric variables are passed", {
+  expect_error(fbposts %>% correlate_partial(type, n_pictures,pop_elite))
+})
