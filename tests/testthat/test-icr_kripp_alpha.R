1e69f8ef77abe3089863df57d8d8830d6942de8a
chainsawriot <chainsawtiney@gmail.com>
2025-07-31 11:44:42 +0200

    Add more tests for krippendorff's alpha

--- tests/testthat/test-icr_kripp_alpha.R
+++ tests/testthat/test-icr_kripp_alpha.R
@@ -49,6 +49,119 @@ test_that("Krippendorff's Alüha returns 1 for variables with one category", {
 
 })
 
+# Numerical accuracy against published values
+
+test_that("Wikipedia example", {
+  # https://en.wikipedia.org/wiki/Krippendorff%27s_alpha#A_computational_example
+
+  wiki <- dget("../testdata/wiki.R")
+  expect_equal(icr_kripp_alpha(wiki), 0.691, tolerance = 0.001)
+  expect_equal(
+    icr_kripp_alpha(wiki, var_level = "interval"),
+    0.811,
+    tolerance = 0.001
+  )
+})
+
+test_that("Krippendorff (2018) examples", {
+  ## Krippendorff (2018) Content analysis: An introduction to its methodology.
+  ## p. 290
+  examples <- dget("../testdata/krippendorff.R")
+
+  ## 1 = book, 2 = letter, 3 = telephone, 4 = computer, 5 = folder
+
+  example1 <- examples[[1]]
+  expect_equal(icr_kripp_alpha(example1), 0.743, tolerance = 0.001)
+  expect_equal(
+    icr_kripp_alpha(example1, var_level = "ordinal"),
+    0.815,
+    tolerance = 0.001
+  )
+  expect_equal(
+    icr_kripp_alpha(example1, var_level = "interval"),
+    0.849,
+    tolerance = 0.001
+  )
+  expect_equal(
+    icr_kripp_alpha(example1, var_level = "ratio"),
+    0.797,
+    tolerance = 0.001
+  )
+
+  ## Example in p. 306
+
+  example2 <- examples[[2]]
+  expect_equal(icr_kripp_alpha(example2), 0.2343, tolerance = 0.001)
+
+  ## Example in p. 310
+  example3 <- examples[[3]]
+  ## in the book, he actually said 0.760, likely due to rounding
+  expect_equal(icr_kripp_alpha(example3), 0.759, tolerance = 0.001)
+})
+
+test_that("Hayes & Krippendorff (2007)", {
+  ## Answering the Call for a Standard Reliability Measure for Coding Data
+  ## https://doi.org/10.1080/19312450709336664
+    hayes <-dget("../testdata/hayes.R")
+    expect_equal(
+        icr_kripp_alpha(hayes, var_level = "ordinal"),
+        0.7598,
+        tolerance = 0.001
+    )
+
+  expect_equal(
+    icr_kripp_alpha(hayes, var_level = "nominal"),
+    0.4765,
+    tolerance = 0.001
+  )
+})
+
+test_that("Freelon (2010)", {
+  # ReCal: Intercoder Reliability Calculation as a Web Service
+  # https://www.ijis.net/ijis5_1/ijis5_1_freelon_pre.html
+  # https://dfreelon.org/publications/2010_ReCal_Intercoder_reliability_calculation_as_a_web_service.pdf
+
+    freelon_matrices <- dget("../testdata/freelon.R")
+    ## Using the reference values from Hayes, not Freelon (Table 9, 10)
+    ## because of numeric precision
+  freelon_alphas <- c(
+    1,
+    0.1929,
+    0.8334,
+    0.8070,
+    1,
+    0.9023,
+    0.9144,
+    0.7928,
+    1,
+    0.8889,
+    0.6354,
+    0.3410,
+    0,
+    0.0278,
+    0.8628,
+    0.7227,
+    0.7002,
+    0.7449,
+    -0.0068,
+    0.7462
+  )
+
+  observed_alphas <- vapply(
+    freelon_matrices,
+    function(x) icr_kripp_alpha(t(x)),
+    numeric(1)
+  )
+  expect_equal(observed_alphas, freelon_alphas, tolerance = 0.001)
+})
+
+test_that("oolong example, bugs in irr", {
+  ## https://github.com/gesistsa/oolong/issues/96
+  chan <- dget("../testdata/chan.R")
+  hand_calculation <- 1 - (4 / ((5 * 25) / 29))
+  expect_equal(icr_kripp_alpha(t(chan)), hand_calculation, tolerance = 0.001)
+})
+
 # Helper functions
 
 test_that("Count value in unit works", {
